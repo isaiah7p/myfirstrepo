a987bd6ea66f194045ed3589356f1772a1904054
qtdevops <qtiot.kumar@gmail.com>
2019-08-22 00:34:36 +0530

    fourth  commit

--- src/main.py
+++ src/main.py
@@ -3,3 +3,53 @@
 # this is second line of code
 
 # this is my third line of code commit 
+
+''' 506  mkdir mygitrepo
+  507  cd mygitrepo/
+  508  clear 
+  509  git --version 
+  510  mkdir myfirstrepo
+  511  cd myfirstrepo/
+  512  clear 
+  513  mkdir src
+  514  mkdir docs
+  515  mkdir test
+  516  git init
+  517  ls
+  518  ls -ldr
+  519  cd .
+  520  ls -lrt
+  521  ls -ldrt
+  522  ls -ldr
+  523  clear 
+  524  git status
+  525  New-Item .\src\main.py
+  526  brew update git
+  527  brew upgrade git
+  528  git status
+  529  git add src/
+  530  git status
+  531  git status
+  532  git config --global user.name qtdevops
+  533  git config --global user.email qtiot.kumar@gmail.com
+  534  git commit -m "this is my first commit"
+  535  git status
+  536  git lg
+  537  git log
+  538  git status
+  539  git add docs/
+  540  git add src/main.py
+  541  git status
+  542  git commit -m "test second commit"
+  543  git status
+  544  git log 
+  545  git status
+  546  git log 
+  547  git status
+  548  git add src/main.py
+  549  git commit -m "test third commit"
+  550  git status
+  551  git log 
+  552  clear 
+  553  history
+  '''
